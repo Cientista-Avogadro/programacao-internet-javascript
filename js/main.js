3d70b9da7174efdfd592eaf0f23b1abd0103a0cf
cientista-avogadro <cientistaavogadro2019@gmail.com>
2021-11-16 18:10:00 +0100

    Trabalhando com Lista

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
-var n1 = 5;
-var n2 = 3;
-var nome = 'cientista';
-var idade = 20;
-var idade2 = 1
-var frase = 'Japão é o melhor time do mundo';
-// alert(nome + ' tem ' + idade + 'anos');
+// var n1 = 5;
+// var n2 = 3;
+// var nome = 'cientista';
+// var idade = 20;
+// var idade2 = 1;
+// var frase = 'Japão é o melhor time do mundo';
+// // alert(nome + ' tem ' + idade + 'anos');
 
-console.log(nome);
-console.log(idade + idade2);
-console.log(n1 * n2)
-console.log(frase.toLocaleLowerCase())
-console.log(frase.replace('Japão','Angola'));
+// console.log(nome);
+// console.log(idade + idade2);
+// console.log(n1 * n2);
+// console.log(frase.toLocaleLowerCase());
+// console.log(frase.replace('Japão', 'Angola'));
 
+var lista = ['maçã', 'pera', 'laranja'];
+lista.push('uva');
+lista.pop();
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join('-'));
+
+var fruta = {
+    nome: 'maçã',
+    cor: 'vermelha'
+}
+
+console.log(fruta)
