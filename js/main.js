f324c2f005d8d46ffbcc7f8b379bfa8ec9c3d399
cientista-avogadro <cientistaavogadro2019@gmail.com>
2021-11-16 17:47:17 +0100

    aula1 introdução

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var n1 = 5;
+var n2 = 3;
+var nome = 'cientista';
+var idade = 20;
+var idade2 = 1
+var frase = 'Japão é o melhor time do mundo';
+// alert(nome + ' tem ' + idade + 'anos');
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(n1 * n2)
+console.log(frase.toLocaleLowerCase())
+console.log(frase.replace('Japão','Angola'));
+
